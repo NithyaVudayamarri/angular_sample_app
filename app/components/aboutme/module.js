dfadb725581036566b653bc01365591c28c61f52
preethampotu <preetham.reddy16@gmail.com>
2019-05-21 18:52:03 -0500

    angular

---
+++ app/components/aboutme/module.js
@@ -0,0 +1,18 @@
+let myapp = angular.module("Myapp",[]);
+let mycontroller = function($scope)
+{
+    $scope.message = "Hello world";
+
+}
+myapp.controller("MyController",mycontroller);
+
+let JSONController = function($scope)
+{
+    let Student = {
+        Fname:"Preetham",
+        Lname:"Potu",
+        Email:"S533713@nwmissouri.edu"
+    };
+    $scope.Student=Student;
+}
+myapp.controller("Student",JSONController);
